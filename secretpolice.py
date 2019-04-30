bc871f1575ce76400c84355b109d3212e3209381
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-04-29 18:10:17 -0700

    Update secretpolice.py

--- secretpolice.py
+++ secretpolice.py
@@ -1,10 +1,21 @@
 # 3.7.2
+
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='$', description='To Enforce the Rule of the Supreme Leader')
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 @bot.command()
-async def test(ctx):
-    await ctx.send("time to end free speech")
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
 
+
+
+bot.run('NTcyNTY2OTYwMDE0MDk4NDcy.XMegcA.8td8bKrohhmd_7XXF5syGiqcHNA')
